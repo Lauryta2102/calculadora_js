f7c8d47b799bbdde5928365add7090198375e747
laura Ibaceta <lauraibaceta@gmail.com>
2023-11-23 06:59:37 -0300

    creacion del proyecto calculadora

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const numeros= document.querySelectorAll(".numero");
+const operadores= document.querySelectorAll(".operador");
+const borrar= document.querySelector(".borrar");
+const igual= document.querySelector(".igual");
+const pantalla= document.querySelector(".pantalla");
+
+let opActual = "";
+let opAnterior = "";
+let resultado = "";
+let operacion = "";
+
+
+operadores.forEach((ope) => {
+    ope.addEventListener("click", () =>{   
+        opAnterior = opActual;
+        opActual = "";
+        operacion = ope.innerHTML;
+    })
+})
+numeros.forEach((numero) => {
+    numero.addEventListener("click", () =>{
+        opActual += numero.innerHTML;
+        pantalla.innerHTML = opActual;
+    })
+
+})
+
+
+
+igual.addEventListener("click", () =>{  
+    switch(operacion){
+        case "+":
+            resultado = parseInt(opAnterior) + parseInt(opActual);
+            break;
+        case "-":
+            resultado = parseInt(opAnterior) - parseInt(opActual);
+            break;
+        case "x":
+            resultado = parseInt(opAnterior) * parseInt(opActual);
+            break;
+        case "÷":
+            resultado = parseInt(opAnterior) / parseInt(opActual);
+            break;
+        case "exp":
+            resultado = parseInt(opAnterior) ** parseInt(opActual);
+            break;
+        default:
+            console.log("Operacion no valida");
+    }
+    pantalla.innerHTML = resultado;
+})
+
+borrar.addEventListener("click", () =>{  
+    pantalla.innerHTML = "";
+    opActual = "";
+})
